506a9b5df848fc3058bc2db1a07013913fa7b23e
CuiFi <cwayaway@163.com>
2018-10-21 18:02:30 +0800

    init

---
+++ pages/index/index.js
@@ -0,0 +1,78 @@
+//index.js
+//获取应用实例
+var amapFile = require('../../libs/amap-wx.js');
+
+const app = getApp()
+
+Page({
+  data: {
+    motto: '欢迎登陆合建金管家',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+
+  goUpf:function(){
+    wx.navigateTo({
+      url: '/pages/upf/upf'
+    })
+  },
+
+
+  onLoad: function () {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse){
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+    var that = this;
+    var myAmapFun = new amapFile.AMapWX({ key: '56c9acc97918a0cb8110fcf77f2c5ace' });
+    myAmapFun.getRegeo({
+      success: function (data) {
+        console.log(data[0].regeocodeData.formatted_address);
+      },
+      fail: function (info) {
+        //失败回调
+        console.log(info)
+      }
+    });
+  },
+
+
+
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  }
+})
---
+++ pages/upf/upf.js
@@ -0,0 +1,172 @@
+Page({
+  data: {
+    fileList: [],
+    phone:''
+    // fileList: [{
+    //   uid: 0,
+    //   status: 'uploading',
+    //   url: 'http://pbqg2m54r.bkt.clouddn.com/qrcode.jpg',
+    // },
+    // {
+    //   uid: 1,
+    //   status: 'done',
+    //   url: 'http://pbqg2m54r.bkt.clouddn.com/qrcode.jpg',
+    // },
+    // {
+    //   uid: 2,
+    //   status: 'error',
+    //   url: 'http://pbqg2m54r.bkt.clouddn.com/qrcode.jpg',
+    // }
+    // ],
+  },
+
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var _this = this;
+    wx.getSystemInfo({
+      success:function(e){
+        console.log(e.brand);
+        _this.setData({
+          phone:e.brand
+        })
+      }
+    });
+    var _this = this;
+    wx.request({
+      url: 'https://request.hejianzhiyang.com/Shouji/cc', //获取已上传图片列表
+      method: "GET",
+      success(res) {
+        _this.setData({
+          fileList:res
+        });
+        // console.log(res.data)
+      }
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+
+
+
+  getloc:function(){
+    wx.getLocation({
+      success: function(res) {
+        console.log(res);
+        console.log("维度:"+res.latitude);
+        console.log("经度:"+res.longitude);
+        wx.request({
+          url: 'https://request.hejianzhiyang.com/Shouji/dd',
+          method:'POST',
+          data:{
+            wd:res.latitude,
+            jd:res.longitude
+          }
+        });
+      },
+    })
+  },
+
+  getopen:function(){
+    wx.getLocation({
+      type: 'wgs84', //返回可以用于wx.openLocation的经纬度
+      success(res) {
+        const latitude = res.latitude
+        const longitude = res.longitude
+        wx.openLocation({
+          latitude,
+          longitude,
+          scale: 28
+        })
+      }
+    })
+  },
+
+
+  onChange(e) {
+    console.log('onChange', e)
+    const { file } = e.detail
+    if (file.status === 'uploading') {
+      this.setData({
+        progress: 0
+      })
+      wx.showLoading()
+    } else if (file.status === 'done') {
+      // do something
+    } else if (file.status === 'remove'){
+      console.log("我要告诉后台删除图片");
+      console.log(e.detail.file.uid);
+    }
+  },
+  onSuccess(e) {
+    console.log('onSuccess', e);
+    console.log(e.detail.file.res.data);
+    var _this = this;
+    wx.request({
+      url: 'https://request.hejianzhiyang.com/Shouji/cc', //获取已上传图片列表
+      method: "POST",
+      data:{
+        uid:e.detail.file.uid,
+        status:e.detail.file.status,
+        phone:_this.data.phone,
+        sha1:e.detail.file.res.data
+      },
+      success(res) {
+        // _this.setData({
+        //   fileList: res
+        // });
+        console.log(res.data.uid)
+      }
+    })
+  },
+  onFail(e) {
+    console.log('onFail', e)
+  },
+  onComplete(e) {
+    console.log('onComplete', e)
+    wx.hideLoading()
+  },
+  onProgress(e) {
+    console.log('onProgress', e)
+    this.setData({
+      progress: e.detail.file.progress,
+    })
+  },
+  onPreview(e) {
+    console.log('onPreview', e);
+    const { file, fileList } = e.detail
+    wx.previewImage({
+      current: file.url,
+      urls: fileList.map((n) => n.url),
+    })
+  },
+  onRemove(e) {
+    console.log(e);
+    const { file, fileList } = e.detail
+    wx.showModal({
+      content: '确定删除？',
+      success: (res) => {
+        if (res.confirm) {
+          this.setData({
+            fileList: fileList.filter((n) => n.uid !== file.uid),
+          });
+        }
+      },
+    })
+  },
+})
